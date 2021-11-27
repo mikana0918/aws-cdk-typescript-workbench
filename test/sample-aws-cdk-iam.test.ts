3b4a8e85538e084333b5de71c2413e77b229d00f
mikana0918 <shogo.kusuhara@gmail.com>
2021-11-27 22:02:23 +0900

    Initial commit

---
+++ test/sample-aws-cdk-iam.test.ts
@@ -0,0 +1,17 @@
+// import { Template } from '@aws-cdk/assertions';
+// import * as cdk from '@aws-cdk/core';
+// import * as SampleAwsCdkIam from '../lib/sample-aws-cdk-iam-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/sample-aws-cdk-iam-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SampleAwsCdkIam.SampleAwsCdkIamStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
